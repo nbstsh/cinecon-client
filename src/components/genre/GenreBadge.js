c93cddf304fd02d5a09d2acff873d2cb93d26ac7
nbstsh <nbstsh@gmail.com>
2019-03-12 17:54:50 +0900

    refactoring GenreBadge style

--- src/components/genre/GenreBadge.js
+++ src/components/genre/GenreBadge.js
@@ -1,14 +1,9 @@
 import React from  'react'
-
+import './GenreBadge.css'
 
 const GenreBadge = ({name, color}) => {
     const style = {
         backgroundColor: color,
-        color: 'var(--color-grey-light-1)',
-        borderRadius: '2px',
-        fontSize: '1.3rem',
-        padding: '.1rem 1rem',
-        transform: 'translateY(10%)'
     }
     return (
         <div className="GenreBadge" style={style}>
