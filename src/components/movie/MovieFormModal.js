eb717497b91765af786e0e45883c5dae95ca91ce
nbstsh <nbstsh@gmail.com>
2019-03-12 23:03:34 +0900

    update MovieDetailContainer: pass id to children strategy

--- src/components/movie/MovieFormModal.js
+++ src/components/movie/MovieFormModal.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
+import ModalControll from '../common/ModalControll'
+import MovieForm from './MovieForm'
+import CreateBtn from '../common/CreateBtn'
 
-class MovieFormModal extends Component {
+class MovieFormModal extends ModalControll {
     constructor(props) {
         super(props)
         this.state = {}
     } 
     render() {
+        const movieForm = <MovieForm 
+            handleCancelClick={this.closeModal}
+            handleAfterSubmit={this.closeModal} />
+                
+        const modal = this.generateModalBoilerplate({
+            id: 'movie-form-model',
+            content: movieForm
+        })
+
         return(
-            <div className='MovieFormModal'>MovieFormModal</div>
+            <div className="MovieFormModal">
+                <CreateBtn handleClick={this.openModal} />
+                {/* {modal} */}
+            </div>
         )
     }
 }
