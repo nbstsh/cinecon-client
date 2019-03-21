ceb7220d93e7f8935da567f2b03920589c427368
nbstsh <nbstsh@gmail.com>
2019-03-22 01:22:31 +0900

    update MovieItem design

--- src/components/movie/MovieItem.js
+++ src/components/movie/MovieItem.js
@@ -8,39 +8,55 @@ class MovieItem extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            title: '',
-            releaseYear: '',
-            genres: ''
+            needShowDetail: false
         }
     } 
     componentDidMount() {
         movieManager.getMovie(this.props.id)
             .then(movie => this.setState(movie))
     }
+    showDetail = () => {
+        this.setState({ needShowDetail: true })
+    }
+    hideDetail = () => {
+        this.setState({ needShowDetail: false })
+    }
     render() {
-        const { title, releaseYear } = this.state
-
+        const {title, director, releaseYear, genres, runningTime, starring, country, needShowDetail} = this.state
         // TODO replace with data
         const img = 'https://upload.wikimedia.org/wikipedia/en/e/e7/Harry_Potter_and_the_Order_of_the_Phoenix_poster.jpg'
      
         return(
-            <div className="MovieItem" onClick={this.props.handleClick}>
-                <figure>
+            // <div className="MovieItem" onClick={this.props.handleClick} >
+            <div className="MovieItem" data-open-detail={needShowDetail}>
+                <figure onClick={this.showDetail}>
                     <img src={img} />
                 </figure>
 
                 <h3><span>{title}</span></h3>
+                <ul onClick={this.hideDetail}>
+                    <li>
+                        <label>director</label>
+                        <span>{director}</span>
+                    </li>
+                    <li>
+                        <label>releaseYear</label>
+                        <span>{releaseYear}</span>
+                    </li>
+                    <li>
+                        <label>runningTime</label>
+                        <span>{runningTime}</span>
+                    </li>
+                    <li>
+                        <label>starring</label>
+                        <span>{starring}</span>
+                    </li>
+                    <li>
+                        <label>country</label>
+                        <span>{country}</span>
+                    </li>
+                </ul>
                 <GenresDisplay  genres={this.state.genres} />
-                
-                {/* <h3>{title}</h3>
-                <label>year:</label>
-                <p>{releaseYear}</p>
-                <label>genre:</label>
-                <GenresDisplay  genres={this.state.genres} /> */}
-
-                {/* <button onClick={this.props.handleClickShowDetail}>
-                    show detail
-                </button> */}
             </div>
         )
     }
